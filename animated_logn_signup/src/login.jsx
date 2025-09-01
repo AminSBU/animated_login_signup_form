6c2fb3be0c79a9cb800d9e9c6f11c8e063de11c2
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-09-01 12:05:55 +0330

    inputs with link and button in login

--- animated_logn_signup/src/login.jsx
+++ animated_logn_signup/src/login.jsx
@@ -1,14 +1,31 @@
-export default Login()
+
+function Login()
 {
     return(
         <>
             <div className="main-container">
                 <div className="login">
-                    
+                    <form action="#">
+                        <h1>Login</h1>
+                        <div className="input-box">
+                            <input type="text" placeholder="username" required></input>
+                            <i className="bx bxs-user"></i>
+                        </div>
+                        <div className="input-box">
+                            <input type="password" placeholder="password" required></input>
+                            <i className="bx bxs-lock-alt"></i>
+                        </div>
+                        <div className="forget-link">
+                            <a href="#">forget password?</a>
+                        </div>
+                        <button type="submit" class="btn">Login</button>
+                    </form>
                 </div>
 
                 <div className="register">
-                    
+                    <form action="#">
+                        
+                    </form>
                 </div>
 
                 <div className="toggle-box">
@@ -18,3 +35,5 @@ export default Login()
         </>
     );
 }
+
+export default Login;
