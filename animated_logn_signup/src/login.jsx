44ee410ef29b602f0b58f7b9d194a92d477f2347
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2025-09-01 11:52:33 +0330

    main structure with div tags

--- animated_logn_signup/src/login.jsx
+++ animated_logn_signup/src/login.jsx
@@ -1,4 +1,20 @@
 export default Login()
 {
-    
+    return(
+        <>
+            <div className="main-container">
+                <div className="login">
+                    
+                </div>
+
+                <div className="register">
+                    
+                </div>
+
+                <div className="toggle-box">
+                    
+                </div>
+            </div>
+        </>
+    );
 }
